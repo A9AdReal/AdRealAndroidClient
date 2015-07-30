8c887ed4aced550ed6ea63d155c503eb8c511e8a
Chengliang <lianguasth@sina.com>
2015-07-30 15:53:17 -0700

    remote the test

---
+++ app/src/main/java/a9/com/a9adsrealandroid/AddPointsView.java
@@ -0,0 +1,99 @@
+package a9.com.a9adsrealandroid;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.PointF;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lianchengliang on 7/26/15.
+ */
+public class AddPointsView extends GLSurfaceView {
+
+    private ArrayList<PointF> mVertices;
+    private static final int MAX_VERT = 4;
+    private AdsImageRenderer mRenderer;
+    float mWidth;
+    float mHeight;
+
+    public void addPoint(PointF p){
+        if(mVertices.size() < MAX_VERT){
+            mVertices.add(p);
+            mRenderer.updateCoord(mVertices);
+        }
+    }
+
+    public AddPointsView(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public AddPointsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+
+
+    private void init(Context context){
+        mVertices = new ArrayList<PointF>();
+
+        // Create an OpenGL ES 2.0 context.
+        setEGLContextClientVersion(2);
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        mRenderer = new AdsImageRenderer(context);
+        //set background to be transparent
+        setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        setZOrderOnTop(true);
+        setRenderer(mRenderer);
+        // Render the view only when there is a change in the drawing data
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_UP) {
+            float x = event.getX()/mWidth;
+            float y = event.getY() / mHeight;
+            Log.e("the location on screen ", "x: " + x + "\t y:" + y + "\n");
+            addPoint(new PointF(x, y));
+        }
+        return true;
+    }
+
+
+    public void pleaseRender(){
+        requestRender();
+    }
+
+    public void resetRegion(){
+        mRenderer.updateCoord(null);
+        mVertices.clear();
+        requestRender();
+    }
+
+    @Override
+    protected void onSizeChanged(int xNew, int yNew, int xOld, int yOld){
+        super.onSizeChanged(xNew, yNew, xOld, yOld);
+        mWidth = xNew;
+        mHeight = yNew;
+    }
+}
---
+++ app/src/main/java/a9/com/a9adsrealandroid/videoRegionSelectActivity.java
@@ -0,0 +1,94 @@
+package a9.com.a9adsrealandroid;
+
+import a9.com.a9adsrealandroid.util.SystemUiHider;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.PointF;
+import android.os.Bundle;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.RelativeLayout;
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class videoRegionSelectActivity extends Activity{
+    private Camera mCamera;
+    private CameraPreview mCameraPreview;
+    private AddPointsView mAddPointsView;
+    private Button bRenderRegion;
+    private Button bReset;
+    private Button bChooseAds;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_vedio_region_select);
+
+        mCamera = getCameraInstance();
+        mCameraPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.cameraLayout);
+        preview.addView(mCameraPreview);
+        //prepare to add points on AddPointsView
+        mAddPointsView = new AddPointsView(this.getApplicationContext());
+        preview.addView(mAddPointsView);
+        //init the button
+        bRenderRegion = new Button(this);
+//        bRenderRegion.setWidth(100);
+        bRenderRegion.setText("Render");
+        bRenderRegion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAddPointsView.pleaseRender();
+            }
+        });
+
+        //add a reset button
+        bReset = new Button(this);
+        bReset.setText("Reset");
+        bReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAddPointsView.resetRegion();
+            }
+        });
+        //
+        GridLayout layoutButton = new GridLayout(this.getApplicationContext());
+        layoutButton.setRowCount(1);
+        layoutButton.setColumnCount(2);
+        layoutButton.addView(bReset);
+        layoutButton.addView(bRenderRegion);
+        //add layout to view
+        FrameLayout.LayoutParams layoutParam = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+        layoutParam.gravity = Gravity.BOTTOM;
+        preview.addView(layoutButton, layoutParam);
+    }
+
+
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+}
